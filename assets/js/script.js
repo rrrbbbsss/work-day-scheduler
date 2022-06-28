166060a253b7ce97db1c176f2707deb0f122fd47
Royce Strange <roycest@swbell.net>
2022-06-28 01:55:38 -0500

    add timer for current day

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,28 @@
 // current day stuff
+var setCurrentDay = function () {
+    $("#currentDay").text(moment().format("dddd, MMMM Do"));
+};
+
+var currentDayTimeout = function () {
+    var currentTime = moment();
+    // moments are mutable so clone currentTime by wrapping it in moment
+    var endOfDay = moment(currentTime).endOf("day");
+    // milliseconds until Next Day
+    var msUntilNextDay = endOfDay.diff(currentTime) + 1;
+    // start timeout
+    setTimeout(() => {
+        setCurrentDay();
+        // set an Interval to update Current Day every 24 hours
+        setInterval(() => setCurrentDay(), 24 * 60 * 60 * 1000)
+    }, msUntilNextDay);
+};
 
 // create elements for timeblocks stuff
 // timers for timeblocks stuff
 // eventhandlers for timeblock stuff
 
 // save/load tasks in timeblocks
+
+// start
+setCurrentDay();
+currentDayTimeout();
